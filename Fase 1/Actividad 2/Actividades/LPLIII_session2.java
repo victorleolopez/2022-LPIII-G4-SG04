ab28947f728d8a837ff2b9d634d3077b36eb7fe0
JOHAN <johan.mamani@ucsm.edu.pe>
2022-08-27 00:29:29 -0500

    Actividad 03 Introduccir datos atravez del teclado

--- Fase 1/Actividad 2/Actividades/LPLIII_session2.java
+++ Fase 1/Actividad 2/Actividades/LPLIII_session2.java
@@ -1,13 +1,25 @@
+import java.util.Scanner;
+
 public class LPLIII_session2 {
     public static void main(String[] args) {
-        // ACTIVIDAD 2
+        // ACTIVIDAD 3
         System.out.println("\t **** Crear el objecto Pelusa **** \t");
         Perro Pelusa = new Perro("pequines", "blanco");
         System.out.println(Pelusa.toString());
-        Pelusa.setTemp(50);
-        Pelusa.setEdad(5);
-        Pelusa.peso = 4;
-        System.out.println("DIAGNOSTICO: El Perro esta " + Diagnostico.aptoVacunaA(Pelusa));
+        double peso, temperatura;
+        int edad;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Ingrese el nuevo peso para el perro : ");
+            peso = sc.nextDouble();
+            Pelusa.peso = peso;
+            System.out.println("Ingrese la nueva edad para el perro : ");
+            edad = sc.nextInt();
+            Pelusa.setEdad(edad);
+            System.out.println("Ingrese la nueva temperatura para el perro : ");
+            temperatura = sc.nextDouble();
+            Pelusa.setTemp(temperatura);
+        }
+        System.out.println("DIAGNOSTICO: El Perro esta " + Diagnostico.aptoVacunaB(Pelusa));
         System.out.println(Pelusa.toString());
     }
 }
