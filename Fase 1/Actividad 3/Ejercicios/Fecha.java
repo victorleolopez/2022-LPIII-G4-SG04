7f1c603e2029edec75da115486ad8e824a6ddd50
JOHAN <johan.mamani@ucsm.edu.pe>
2022-09-01 23:19:44 -0500

    Avance del ejercicio 02 de fecha :)

---
+++ Fase 1/Actividad 3/Ejercicios/Fecha.java
@@ -0,0 +1,108 @@
+public class Fecha {
+
+    private int dia;
+    private int mes;
+    private int año;
+
+    // Constructor por defecto
+    public Fecha() {
+        this.dia = 01;
+        this.mes = 01;
+        this.año = 2000;
+    }
+
+    // Constructor con parámetros
+    public Fecha(int dia, int mes, int año) {
+        this.dia = dia;
+        this.mes = mes;
+        this.año = año;
+    }
+
+    // setters y getters
+    public void setDia(int d) {
+        dia = d;
+    }
+
+    public void setMes(int m) {
+        mes = m;
+    }
+
+    public void setAño(int a) {
+        año = a;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    // Método para comprobar si la fecha es correcta
+    public boolean fechaCorrecta() {
+        boolean diaCorrecto, mesCorrecto, añoCorrecto;
+        añoCorrecto = año > 0;
+        mesCorrecto = mes >= 1 && mes <= 12;
+        switch (mes) {
+            case 2:
+                if (esBisiesto()) {
+                    diaCorrecto = dia >= 1 && dia <= 29;
+                } else {
+                    diaCorrecto = dia >= 1 && dia <= 28;
+                }
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                diaCorrecto = dia >= 1 && dia <= 30;
+                break;
+            default:
+                diaCorrecto = dia >= 1 && dia <= 31;
+        }
+        return diaCorrecto && mesCorrecto && añoCorrecto;
+    }
+
+    // Método privado para comprobar si el año es bisiesto
+    // Este método lo utiliza el método fechaCorrecta
+    private boolean esBisiesto() {
+        return (año % 4 == 0 && año % 100 != 0 || año % 400 == 0);
+    }
+
+    // Método que modifica la fecha cambiándola por la del día siguiente
+    public void diaSiguiente() {
+        dia++;
+        if (!fechaCorrecta()) {
+            dia = 1;
+            mes++;
+            if (!fechaCorrecta()) {
+                mes = 1;
+                año++;
+            }
+
+        }
+    }
+
+    // Método toString para mostrar la fecha
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        if (dia < 10) {
+            sb.append("0");
+        }
+        sb.append(dia);
+        sb.append("-");
+        if (mes < 10) {
+            sb.append("0");
+        }
+        sb.append(mes);
+        sb.append("-");
+        sb.append(año);
+        return sb.toString();
+    }
+} // Fin de la clase Fecha
