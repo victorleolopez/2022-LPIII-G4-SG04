bc4a919b7f8fc76429118015e1affd78a0d3ad9f
JOHAN <johan.mamani@ucsm.edu.pe>
2022-09-28 17:27:03 -0500

    Actividades Completas Laboratorio 06

---
+++ Fase 1/Actividad 6/Actividades/Prestamo.java
@@ -0,0 +1,36 @@
+public class Prestamo implements PorPagar {
+    private int numeroDeCuotas;
+    private double montoDeCuota;
+
+    public Prestamo(int numeroDeCuotas, double montoDeCuota) {
+        setNumeroDeCuotas(numeroDeCuotas);
+        setMontoDeCuota(montoDeCuota);
+    }
+
+    public int getNumeroDeCuotas() {
+        return numeroDeCuotas;
+    }
+
+    public void setNumeroDeCuotas(int numeroDeCuotas) {
+        this.numeroDeCuotas = numeroDeCuotas;
+    }
+
+    public double getMontoDeCuota() {
+        return montoDeCuota;
+    }
+
+    public void setMontoDeCuota(double montoDeCuota) {
+        this.montoDeCuota = montoDeCuota;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Prestamo:\nNumero de Cuotas: %s\nMonto de Cuotas: $%,.2f", numeroDeCuotas,montoDeCuota);
+    }
+
+    @Override
+    public double getMontoPago() {
+        return getNumeroDeCuotas() * getMontoDeCuota();
+    }
+
+}
--- Fase 1/Actividad 6/Actividades/PruebaInterfazPorPagar.java
+++ Fase 1/Actividad 6/Actividades/PruebaInterfazPorPagar.java
@@ -1,10 +1,12 @@
 public class PruebaInterfazPorPagar {
     public static void main(String[] args) {
-        PorPagar[] objetoPorPagar = new PorPagar[4];
+        PorPagar[] objetoPorPagar = new PorPagar[6];
         objetoPorPagar[0] = new Factura("01234", "asieno", 2, 375.00);
         objetoPorPagar[1] = new Factura("56789", "llanta", 4, 79.95);
         objetoPorPagar[2] = new EmpleadoAsalariado("Johan", "Mamani", "111-11-1111", 800.00);
         objetoPorPagar[3] = new EmpleadoAsalariado("Lisa", "Barnes", "888-88-8888", 1200.00);
+        objetoPorPagar[4] = new Prestamo(5,500);
+        objetoPorPagar[5] = new Prestamo(3,600);
         System.out.println("\nFacturas y Empleado procesados en forma polimorfica:\n");
         for (PorPagar porPagarActual : objetoPorPagar) {
             System.out.printf("%s\n%s: $%,.2f\n\n", porPagarActual.toString(), "pago vencido",
